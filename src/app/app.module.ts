03f930b94ec72ca67262d4ec127199a00404d95a
Rodolpho <redsmedeiros@gmail.com>
2022-11-04 11:57:08 -0300

    commit dia 04-11 as 11:56

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { AuthGuard } from './guard/auth.guard';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +30,8 @@ import { UsuariosFormComponent } from './usuarios/usuarios-form/usuarios-form.co
     HttpClientModule,
     BrowserAnimationsModule,
     ModalModule.forRoot(),
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
--- src/app/usuarios/usuarios-form/usuarios-form.component.ts
+++ src/app/usuarios/usuarios-form/usuarios-form.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuarios-form',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosFormComponent implements OnInit {
 
-  constructor() { }
+  public form!: FormGroup
+
+  constructor(private fb: FormBuilder, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+
+    this.form = this.fb.group(
+      {
+        nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]],
+        email: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+        senha: [null, [Validators.minLength(3)], Validators.maxLength(10)],
+        setor: [null, [Validators.minLength(3)]],
+        ativo: [null]
+
+      }
+    )
+  }
+
+  retornar(): void{
+
+    this.router.navigate(['/usuarios'])
+
   }
 
 }
